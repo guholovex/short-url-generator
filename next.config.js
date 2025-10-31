d0e51fb3465e4f2a72a57483d47e03bdf6d3c869
XIN <55569151+guholovex@users.noreply.github.com>
2025-10-31 14:41:43 +0800

    Convert next.config.ts to next.config.js

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // 无需 experimental（pg 已兼容）
+};
+
+module.exports = nextConfig;
--- next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  // 无需 experimental（pg 兼容）
-};
-
-export default nextConfig;
