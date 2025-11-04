9f785d8523b7fb5e1060b5f9c5cf4ad9219bdff9
guholovex <l2276227464@163.com>
2025-11-04 13:27:24 +0800

    fix: avoid 404

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,8 @@ export default createMiddleware(routing);
 export const config = {
   //匹配所有路径
   matcher: [
-    '/((?!api|_next|_vercel|.*\\..*|/|index.html).*)', // 排除 / 和 index.html
-    '/([\\w-]+)?/(.+)', // 子路径如 [shortcode]、shorten],
+    // 排除根 / 和静态/API，避免拦截首页
+    '/((?!api|_next|_vercel|.*\\..*|/|index.html).*)',
+    '/([\\w-]+)?/(.+)', // 只匹配子路径，如 [shortcode]、shorten
   ],
 };
--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,12 @@ const withNextIntl = createNextIntlPlugin({
 });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    webpackBuildWorker: true, // 消除警告
+  },
+  // Vercel 优化：启用 standalone 模式
+  output: 'standalone', // 推荐：Vercel 服务器less 兼容
+};
 
 export default withNextIntl(nextConfig);
